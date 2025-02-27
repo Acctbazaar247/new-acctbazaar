63b4a647ef08172503ecaf4b8b1d401730b4ce47
naimur53 <naimurrhman53@gmail.com>
2025-02-27 20:56:27 +0600

    eo

--- src/shared/btcOption.tsx
+++ src/shared/btcOption.tsx
@@ -1,6 +1,32 @@
 // on label add div ith image and text after it
 export const oxDepositOption=[
     {
+        label: (
+          <div className="flex items-center gap-2">
+            <img
+              src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/vnnmkadmkte0gh9p4dof.png"
+              alt="BTC"
+              className="w-6 h-6"
+            />
+            <span>BTC</span>
+          </div>
+        ),
+        value: "BTC",
+      },
+      {
+        label: (
+          <div className="flex items-center gap-2">
+            <img
+              src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/h9znrkx1c5kdpkhqxtdd.png"
+              alt="ETH"
+              className="w-6 h-6"
+            />
+            <span>ETH</span>
+          </div>
+        ),
+        value: "ETH",
+      },
+    {
       label: (
         <div className="flex items-center gap-2">
           <img
@@ -13,32 +39,34 @@ export const oxDepositOption=[
       ),
       value: "LTC",
     },
-    // {
-    //   label: (
-    //     <div className="flex items-center gap-2">
-    //       <img
-    //         src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663510/jdfgbvhrywwqi8gmqzos.png"
-    //         alt="BUSD"
-    //         className="w-6 h-6"
-    //       />
-    //       <span>BUSD</span>
-    //     </div>
-    //   ),
-    //   value: "BUSD",
-    // },
-    // {
-    //   label: (
-    //     <div className="flex items-center gap-2">
-    //       <img
-    //         src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663510/zvifybdbq4gwdof5bbc2.png"
-    //         alt="SOL"
-    //         className="w-6 h-6"
-    //       />
-    //       <span>SOL</span>
-    //     </div>
-    //   ),
-    //   value: "SOL",
-    // },
+ 
+    {
+        label: (
+          <div className="flex items-center gap-2">
+            <img
+              src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740667349/komp6yeu8lyjbmd7gyy4.png"
+              alt="USDT - TRC-20"
+              className="w-6 h-6"
+            />
+            <span>USDT - TRC-20</span>
+          </div>
+        ),
+        value: "USDT (TRC20)",
+      },
+
+    {
+        label: (
+          <div className="flex items-center gap-2">
+            <img
+              src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/fbf4ndmub6td4rbvnbjn.png"
+              alt="USDT - BEP-20"
+              className="w-6 h-6"
+            />
+            <span>USDT - BEP-20</span>
+          </div>
+        ),
+        value: "USDT (BEP20)",
+      },
     {
       label: (
         <div className="flex items-center gap-2">
@@ -65,45 +93,7 @@ export const oxDepositOption=[
       ),
       value: "TRX",
     },
-    {
-      label: (
-        <div className="flex items-center gap-2">
-          <img
-            src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/h9znrkx1c5kdpkhqxtdd.png"
-            alt="ETH"
-            className="w-6 h-6"
-          />
-          <span>ETH</span>
-        </div>
-      ),
-      value: "ETH",
-    },
-    {
-      label: (
-        <div className="flex items-center gap-2">
-          <img
-            src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/fbf4ndmub6td4rbvnbjn.png"
-            alt="USDT - TRC-20"
-            className="w-6 h-6"
-          />
-          <span>USDT - TRC-20</span>
-        </div>
-      ),
-      value: "USDT (TRC20)",
-    },
-    {
-      label: (
-        <div className="flex items-center gap-2">
-          <img
-            src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/fbf4ndmub6td4rbvnbjn.png"
-            alt="USDT - BEP-20"
-            className="w-6 h-6"
-          />
-          <span>USDT - BEP-20</span>
-        </div>
-      ),
-      value: "USDT (BEP20)",
-    },
+  
     // {
     //   label: (
     //     <div className="flex items-center gap-2">
@@ -132,18 +122,18 @@ export const oxDepositOption=[
     },
     
     {
-      label: (
-        <div className="flex items-center gap-2">
-          <img
-            src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663955/ayqqa7k0jwxunfuccnnp.png"
-            alt="USDC - BEP-20"
-            className="w-6 h-6"
-          />
-          <span>USDC - BEP-20</span>
-        </div>
-      ),
-      value: "USDC (BEP20)",
-    },
+        label: (
+          <div className="flex items-center gap-2">
+            <img
+              src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663955/ayqqa7k0jwxunfuccnnp.png"
+              alt="USDC - BEP-20"
+              className="w-6 h-6"
+            />
+            <span>USDC - BEP-20</span>
+          </div>
+        ),
+        value: "USDC (BEP20)",
+      },
     // {
     //   label: (
     //     <div className="flex items-center gap-2">
@@ -170,18 +160,6 @@ export const oxDepositOption=[
       ),
       value: "USDC (ERC20)",
     },
-    {
-      label: (
-        <div className="flex items-center gap-2">
-          <img
-            src="https://res.cloudinary.com/dfyeocma8/image/upload/v1740663511/vnnmkadmkte0gh9p4dof.png"
-            alt="BTC"
-            className="w-6 h-6"
-          />
-          <span>BTC</span>
-        </div>
-      ),
-      value: "BTC",
-    },
+
   ];
   
